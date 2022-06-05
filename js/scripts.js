5fcc261954aecb7c2dd876163a2e0ca5b5702c17
ryandefea <ryan.defea@gmail.com>
2022-06-05 16:58:30 -0700

    refactor if else statement.

--- js/scripts.js
+++ js/scripts.js
@@ -18,33 +18,22 @@ function divide(number1, number2) {
 
 // User interface logic
 
-  $(document).ready(function() {
-    $("form#add").submit(function(event) {
-      event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
+$(document).ready(function() {
+  $("form#calculator").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
-    });
-    $("form#sub").submit(function(event) {
-      event.preventDefault();
-    const number1 = parseInt($("#sub1").val());
-    const number2 = parseInt($("#sub2").val());
-    const result = subtract(number1, number2);
-    $("#output1").text(result);
-    });
-    $("form#multi").submit(function(event) {
-      event.preventDefault();
-    const number1 = parseInt($("#multi1").val());
-    const number2 = parseInt($("#multi2").val());
-    const result = multiply(number1, number2);
-    $("#output2").text(result);
-    });
-    $("form#divide").submit(function(event) {
-      event.preventDefault();
-    const number1 = parseInt($("#divide1").val());
-    const number2 = parseInt($("#divide2").val());
-    const result = divide(number1, number2);
-    $("#output3").text(result);
-    });
+  });
 });
